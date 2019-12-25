ad3e1d4e728f2180f54b002368eb5edb88075d30
Deikun95 <Deikun95@gmail.com>
2019-12-26 02:40:32 +0300

    Сверстал слайдер, настроил селект. Почти доделал сайт.

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(window).load(function () {
+    $(".before-after").twentytwenty({
+        before_label: 'Без скинали',
+        after_label: 'Со скинали'
+    });
+    $('.before-slider').slick({
+        draggable: false,
+        dots: true,
+        dotsClass: 'before-slider__dots',
+        prevArrow: $('.arrow-left'),
+        nextArrow: $('.arrow-right')
+    });
+    $('.menu-button').on('click', function () {
+        $('.menu').toggleClass('menu_active')
+    });
+
+    $('.select_checked').on('click', function(){
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+    $('.select__option').on('click', function(){
+        let value = $(this).attr('data-value');
+        $('#select-type').val(value);
+        $('.select_checked').text(value);
+        $('.select__dropdown').toggleClass('select__dropdown_open');
+    });
+});
